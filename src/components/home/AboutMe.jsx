87546df1811c8c82f702577cb33449d97b06d060
himalmidun <himalmidun9@gmail.com>
2025-01-02 10:02:37 +0000

    ppp

--- src/components/home/AboutMe.jsx
+++ src/components/home/AboutMe.jsx
@@ -56,7 +56,8 @@ const AboutMe = ({ heading, message, link, imgSize, resume }) => {
             <p className="lead text-center">
               <a
                 className="btn btn-outline-dark btn-lg"
-                href={resume}
+                // href={resume}
+                href="/myresume.pdf"
                 // target="_blank"
                 // rel="noreferrer noopener"
                 role="button"
