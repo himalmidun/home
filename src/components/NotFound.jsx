46a8a36d0f4ca070c05a357806523cdeb7518d58
himalmidun <himalmidun9@gmail.com>
2025-01-05 07:22:15 +0000

    final error page

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -5,9 +5,39 @@ const NotFound = React.forwardRef((props, ref) => {
     console.log("Rendering NotFound Component");
     return (
         <Jumbotron >
-            <div style={{ textAlign: "center", padding: "50px", margin:0 }}>
-                <h1 ref={ref}>404 - Page Not Found</h1>
-                <a href="/" style={{ color: "blue", textDecoration: "underline" }}>
+            <div style={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                padding: "50px",
+                margin: 0
+            }}>
+                <img src="/lost.png" alt="Error 404" style={{
+                    maxWidth: "100%",
+                    height: "auto",
+                    maxHeight: "430px",
+                    width: "auto",
+                    position: "relative"
+                }} />
+                <h1 ref={ref} style={{ marginTop: '20px', fontSize: '5em' }}>404 - Page Not Found</h1>
+                {/* <a href="/" style={{ color: "blue", textDecoration: "underline" }}>
+                    Go back home
+                </a> */}
+
+                <a href="/" style={{
+                    color: "#007bff",
+                    textDecoration: "none",
+                    fontSize: "20px",
+                    fontWeight: "bold",
+                    position: "relative",
+                    display: "inline-flex",
+                    alignItems: "center",
+                    marginTop: 50,
+                }}>
+                    <i class="fa fa-home" style={{
+                        marginRight: "8px",
+                        transition: "transform 0.3s ease"
+                    }}></i>
                     Go back home
                 </a>
             </div>
